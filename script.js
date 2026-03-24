0c627660da3642a78949bda0744089ca47b60423
Enzo Bariotto <enzobariotto@gmail.com>
2026-03-24 10:51:19 -0300

    Ajustando o CSS e a API para mostrar o tipo e o Status do pokemon

--- script.js
+++ script.js
@@ -42,8 +42,61 @@ async function mostrarTudo(url) {
         
     };
 
+    async function mostrarTipo(url) {
+        const res = await fetch(url);
+        const dados = await res.json();
+
+        const tipo = dados.types[0].type.name;
+
+        document.querySelector("#tipoPokemon").innerHTML = `
+        <strong>Tipo:</strong><p>${tipo}</p>
+    `;
+        
+    };
+
+    async function mostrarHp(url) {
+        const res = await fetch(url);
+        const dados = await res.json();
+
+        const hp = dados.stats[0].base_stat;
+
+        document.querySelector("#hpPokemon").innerHTML = `
+        <strong>HP:</strong><p>${hp}</p>
+    `;
+        
+    };
+
+     async function mostrarAtaque(url) {
+        const res = await fetch(url);
+        const dados = await res.json();
+
+        const ataque = dados.stats[1].base_stat;
+
+        document.querySelector("#ataquePokemon").innerHTML = `
+        <strong>Ataque:</strong><p>${ataque}</p>
+    `;
+        
+    };
+
+     async function mostrarDefesa(url) {
+        const res = await fetch(url);
+        const dados = await res.json();
+
+        const defesa = dados.stats[2].base_stat;
+
+        document.querySelector("#defesaPokemon").innerHTML = `
+        <strong>Defesa:</strong><p>${defesa}</p>
+    `;
+        
+    };
+
+
     mostrarImagem(url);
     mostrarHabilidade(url);
+    mostrarTipo(url);
+    mostrarHp(url);
+    mostrarAtaque(url);
+    mostrarDefesa(url);
 }
 
 
